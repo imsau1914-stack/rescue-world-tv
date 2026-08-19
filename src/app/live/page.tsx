9cc11e97f7878cc68410ffea8d25fb066d48db5c
imsau1914-stack <imsau1914@gmail.com>
2026-08-19 12:11:26 -0400

    Create live page .tsx

---
+++ src/app/live/page.tsx
@@ -0,0 +1,9 @@
+export default function LivePage() {
+  return (
+    <main style={{ padding: "80px 40px", color: "white"}}>
+      <h1>LIVE</h1>
+      <h2>Rescue World TV</h2>
+      <p>Watch our live broadcast 24/7.</p>
+    </main>
+  );
+}  
